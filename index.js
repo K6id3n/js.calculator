6e2e9f3ac034ccbd200518860d93912a427a4b2d
K6id3n <143832452+K6id3n@users.noreply.github.com>
2024-01-24 09:51:03 -0800

    almost woprks

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-function inp(){
+function inp() {
 
-    let equation = prompt("Please enter the equation you would like to solve!", "");
-
-    
-
-    
+    let equationType = prompt("Please enter the equation you would like to solve!", "");
+    switch (equationType) {
+        case "Addition":
+            add();
+            break;
+        case "Subtraction":
+            subtract();
+            break;
+        case "Multiplication":
+            multiply();
+            break;
+        case "Division":
+            divide();
+            break;
+        default:
+            alert("Error")
+    }
 
 }
+
+function add(){
+    let x = a + b 
+}
+function subtract(){
+    let x = a - b
+}
+function multiply(){
+    let x = a * b
+}
+function divide(){
+    let x = a / b
+}
+
+x = 10
+document.getElementById("out").alert = x;
