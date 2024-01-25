66d56b873b5856adbac07023faa4d74b3ce37d2f
K6id3n <kaidengarza203@gmail.com>
2024-01-25 12:35:25 -0800

    CSS changes

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 function inp() {
-    const display = prompt("Enter Equation","")
-    const splitDisplay = display.split('+');
-    if (splitDisplay.length === 2){
-        
+    const display = prompt("Enter Equation", "")
+    const splitDisplay = display.split(/[\+\-\*\/]/);
+    if (splitDisplay.length === 2) {
+
         const n1 = parseFloat(splitDisplay[0]);
         const n2 = parseFloat(splitDisplay[1]);
-        
+
         console.log(`n1: ${n1}, n2: ${n2}`)
         checkValue(n1, n2);
+    } else if(isNaN(myVariable)) {
+        alert("NO NUMERICAL VALUE")
     } else {
         console.log('INVALID EQUATION FORMAT')
     }
-    function checkValue(n1, n2){
-        if (display.includes("+")){
+    function checkValue(n1, n2) {
+        if (display.includes("+")) {
             let result = (n1 + n2)
             alert(result)
-        } else if (display.includes("-")){
+
+        } else if (display.includes("-")) {
             let result = (n1 - n2)
             alert(result)
 
-        } else if (display.includes("*")){
+        } else if (display.includes("*")) {
             let result = (n1 * n2)
             alert(result)
 
-        } else if (display.includes("/")){
+        } else if (display.includes("/")) {
             let result = (n1 / n2)
             alert(result)
 
@@ -32,4 +35,4 @@ function inp() {
         }
 
     }
-}99
+}
